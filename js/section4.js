bb780983688ba3849bb00ce5194bb94eb200d3c8
Piotr20 <piotrpospiech00@gmail.com>
2020-02-26 02:57:20 +0100

    sth

--- js/section4.js
+++ js/section4.js
@@ -13,7 +13,7 @@ window.addEventListener('scroll', function () {
                this.setTimeout(
                     () => {
                          div[i].classList.add("active1");
-                    }, i * 400);
+                    }, i * 300);
                this.setTimeout(() => {
                     div[i].classList.add("active2");
                }, 5500)
@@ -23,25 +23,40 @@ window.addEventListener('scroll', function () {
 
           }
           div[0].addEventListener('click', () => {
-               div[0].classList.toggle('active4');
+               div[0].classList.add('active4');
                div[0].classList.remove('active1');
 
           });
           div[1].addEventListener('click', () => {
-               div[1].classList.toggle('active4');
+               div[1].classList.add('active4');
                div[1].classList.remove('active1');
           });
           div[2].addEventListener('click', () => {
-               div[2].classList.toggle('active4');
+               div[2].classList.add('active4');
                div[2].classList.remove('active1');
           });
           div[3].addEventListener('click', () => {
-               div[3].classList.toggle('active4');
+               div[3].classList.add('active4');
                div[3].classList.remove('active1');
           });
           div[4].addEventListener('click', () => {
-               div[4].classList.toggle('active4');
+               div[4].classList.add('active4');
                div[4].classList.remove('active1');
+          });
+
+          let icon = document.querySelectorAll(".animation_container div i");
+          icon[0].addEventListener('click', () => {
+               div[0].classList.remove('active4');
+               div[0].addEventListener('click', () => {
+                    div[0].classList.toggle('active4');
+                    icon[0].style.fontSize = '0' + 'px';
+                    icon[0].style.transitionDelay = "0s";
+               });
+
+          });
+          div[0].addEventListener('click', () => {
+               icon[0].style.fontSize = '100' + 'px';
+
           });
      } else {
           console.log('Not in the viewport... whomp whomp');
